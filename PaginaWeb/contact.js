863c2d9ae2f62247f8f28ce683a2fba3a4b5091f
angelcorreav <holaf00321@gmail.com>
2021-12-09 13:17:40 -0500

    first commit

---
+++ PaginaWeb/contact.js
@@ -0,0 +1,3 @@
+let resultado = window.confirm('Cordial Saludo querido usuario bienvenido  a esta pagina en la cual se podrá encontrar una pequeña asesoría de los diferentes generos de anime que existen ¡Espero que disfrutes esta página!')
+if (resultado === true){    
+}
